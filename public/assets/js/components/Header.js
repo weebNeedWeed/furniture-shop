43f18579525737c475a1247371278ed1fc7e89a7
Weed <rivenmle@gmail.com>
2022-04-25 17:35:00 +0700

    -fix: changed filename home.html to index.html (site didnot work without index)

--- public/assets/js/components/Header.js
+++ public/assets/js/components/Header.js
@@ -15,14 +15,14 @@ headerTemplate.innerHTML = `
   <header>
     <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
       <div class="container-fluid">
-        <a class="navbar-brand" href="./home.html">Furnitures Shop</a>
+        <a class="navbar-brand" href="./index.html">NVM</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="mynavbar">
           <ul class="navbar-nav me-auto">
             <li class="nav-item">
-              <a class="nav-link" href="./home.html">Home</a>
+              <a class="nav-link" href="./index.html">Home</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="./cart.html">Cart</a>

ebf7f71c3c67a8a4f8ee8beec31278d45e77b69f
huynhviethung214 <huynhviethung88202@gmail.com>
2022-05-05 16:46:19 +0700

    Fix front page html and css
    -fix: create preview.html and css and move code from home.html and css to it
    -fix: change homepage design
    -feat: make text in header and homepage none selectable
    -fix: change home.js, home.html and home.css to preview.js, preview.html and preview.css
    -feat: add images for homepage design

--- public/assets/js/components/Header.js
+++ public/assets/js/components/Header.js
@@ -2,18 +2,22 @@ const headerTemplate = document.createElement("template");
 
 headerTemplate.innerHTML = `
   <style>
+    .navbar {
+      background-color: #0a0c0a;
+    }
+
     .nav-bar-buttons > * {
       margin-right: 10px;
       border: none;
-      color: #6c757d;
-      background-color: #212529;
+      color: #d9d6d8;
+      background-color: #0a0c0a;
       font-size: 14px;
       font-family: Roboto;
     }
   </style>
 
   <header>
-    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+    <nav class="navbar navbar-expand-sm navbar-dark">
       <div class="container-fluid">
         <a class="navbar-brand" href="./index.html">NVM</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
@@ -23,6 +27,9 @@ headerTemplate.innerHTML = `
           <ul class="navbar-nav me-auto">
             <li class="nav-item">
               <a class="nav-link" href="./index.html">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="./preview.html">Preview</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="./cart.html">Cart</a>

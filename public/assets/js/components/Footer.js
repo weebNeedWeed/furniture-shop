5869f4d03f3da05b6b5206593eb685aab418a527
Weed <rivenmle@gmail.com>
2022-05-08 17:32:52 +0700

    feat: added footer

--- public/assets/js/components/Footer.js
+++ public/assets/js/components/Footer.js
@@ -1,8 +1,62 @@
 const footerTemplate = document.createElement("template");
 
 footerTemplate.innerHTML = `
-  <footer>
-    footer
+  <footer class="footer">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-4">
+          <a href="/index.html" class="footer__logo">NVM</a>
+          <ul class="contact">
+            <li class="contact__logo contact__logo--facebook">
+              <i class="fa-brands fa-facebook"></i>
+            </li>
+            <li class="contact__logo contact__logo--twitter">
+              <i class="fa-brands fa-twitter"></i>
+            </li>
+            <li class="contact__logo contact__logo--instagram">
+              <i class="fa-brands fa-instagram"></i>
+            </li>
+          </ul>
+        </div> 
+        <div class="col-md-8">
+          <div class="row h-100">
+            <div class="col-md-3 col-sm-12">
+              <ul class="footer-nav">
+                <li class="footer-nav__item">
+                  <a href="/index.html" class="footer-nav__link">Trang chủ</a>
+                </li>
+              </ul>
+            </div>
+            
+            <div class="col-md-3 col-sm-12">
+              <ul class="footer-nav">
+                <li class="footer-nav__item">
+                  <a href="/preview.html" class="footer-nav__link">Sản phẩm</a>
+                </li>
+              </ul>
+            </div>
+            
+            <div class="col-md-3 col-sm-12">
+              <ul class="footer-nav">
+                <li class="footer-nav__item">
+                  <a href="/cart.html" class="footer-nav__link">Giỏ hàng</a>
+                </li>
+              </ul>
+            </div>
+
+            <div class="col-md-3 col-sm-12">
+              <ul class="footer-nav">
+                <li class="footer-nav__item">
+                  <a href="/login.html" class="footer-nav__link">Đăng nhập</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <p class="footer__copyright">Copyright &copy; 2022 Bản quyền thuộc về NVM.</p>
+    </div>
   </footer>
 `;
 

4639016c76bd7883f7b7a8b0ab4e3fc704268c76
Weed <rivenmle@gmail.com>
2022-05-13 21:21:37 +0700

    -feat: added responsive design for cart
    -feat: added 'add to cart' alert in preview

--- public/assets/js/cart.js
+++ public/assets/js/cart.js
@@ -1,20 +1,20 @@
 const amounts = document.getElementsByClassName("amounts")[0];
 const amount_items = document.getElementsByClassName("amount-items")[0];
-const amount_inputs = document.getElementsByClassName('amount-input');
+const amount_inputs = document.getElementsByClassName("amount-input");
 var sum = 0;
 
 function checkValidAmount(element) {
-	if (element.value < 1 || element.value == ' ') {
+	if (element.value < 1 || element.value == " ") {
 		element.value = 1;
 	}
 }
 
 function removeItem(item_id) {
 	let products = document.getElementsByClassName("products")[0];
-	Array.from(amount_inputs).forEach(element => {
+	Array.from(amount_inputs).forEach((element) => {
 		if (element.id == item_id) {
 			products.removeChild(element.parentElement.parentElement.parentElement);
-			localStorage.removeItem(item_id.replace(/-/g, ' '));
+			localStorage.removeItem(item_id.replace(/-/g, " "));
 			calculateInfo();
 		}
 	});
@@ -34,7 +34,7 @@ function createItem(item_name) {
 
 	let product_name = document.createElement("h3");
 	product_name.className = "product-name";
-	product_name.innerHTML = `Tên hàng: ${item_name}`;
+	product_name.innerHTML = `Tên hàng: ${shopItem.vietnameseName}`;
 
 	let product_price = document.createElement("h4");
 	product_price.className = "product-price";
@@ -43,15 +43,48 @@ function createItem(item_name) {
 
 	let product_quantity = document.createElement("p");
 	product_quantity.className = "product-quantity";
-	product_quantity.innerHTML = `Số lượng: <input min=\"1\" type=\"number\"  id=${item_name.replace(/\s/g, '-')} class=\"amount-input\" value=\"1\">`;
-	let _input = product_quantity.getElementsByTagName('input')[0];
-	_input.addEventListener('input', function() {
+	product_quantity.innerHTML = `
+    Số lượng: 
+    <button class="adjust-quantity-btn" >
+      <i class="fa-solid fa-plus"></i>
+    </button>
+    <input min=\"1\" type=\"number\"  id=${item_name.replace(
+			/\s/g,
+			"-",
+		)} class=\"amount-input\" value=\"1\" disabled>
+    <button class="adjust-quantity-btn" >
+      <i class="fa-solid fa-minus"></i>
+    </button>
+  `;
+	let _input = product_quantity.getElementsByTagName("input")[0];
+	_input.addEventListener("input", function () {
 		checkValidAmount(_input);
 	});
 
+	// Handle user change quantity
+	const handleIncrementQuantity = () => {
+		_input.value = parseInt(_input.value, 10) + 1;
+		calculateInfo();
+	};
+	product_quantity.getElementsByTagName("button")[0].onclick =
+		handleIncrementQuantity;
+
+	const handleDecrementQuantity = () => {
+		const oldValue = parseInt(_input.value, 10);
+		const newValue = oldValue - 1 >= 1 ? oldValue - 1 : 1;
+
+		_input.value = newValue;
+
+		calculateInfo();
+	};
+	product_quantity.getElementsByTagName("button")[1].onclick =
+		handleDecrementQuantity;
+
 	let product_remove = document.createElement("p");
 	product_remove.className = "product-remove";
-	product_remove.onclick = function() { removeItem(item_name.replace(/\s/g, '-')) };
+	product_remove.onclick = function () {
+		removeItem(item_name.replace(/\s/g, "-"));
+	};
 
 	let product_remove_icon = document.createElement("i");
 	product_remove_icon.className = "fa fa-trash";
@@ -117,8 +150,8 @@ function calculateInfo() {
 	sum = 0;
 	var n_items = 0;
 
-	Array.from(amount_inputs).forEach(element => {
-		var item_name = element.id.replace(/-/g, ' ');
+	Array.from(amount_inputs).forEach((element) => {
+		var item_name = element.id.replace(/-/g, " ");
 		let shopItem = SHOPITEMS.find((elm) => elm.name === item_name);
 
 		n_items += parseInt(element.value);
@@ -128,8 +161,11 @@ function calculateInfo() {
 	amounts.innerHTML = `${formatVND(sum)} VND`;
 }
 
-Array.from(amount_inputs).forEach(element => {
-	element.addEventListener("input", debounce(() => {
-		calculateInfo();
-	}));
-})
+Array.from(amount_inputs).forEach((element) => {
+	element.addEventListener(
+		"input",
+		debounce(() => {
+			calculateInfo();
+		}),
+	);
+});
